fef916386db29fa84d5f595f36aea8d30687b9b9
unknown <scander.bou@gmail.com>
2021-08-10 19:01:57 +0200

    skander chez adel

--- app/Bateau.php
+++ app/Bateau.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Client;
 
 class Bateau extends Model
 {
      protected $fillable = [
         'nom', 'type', 'indicatif','poid','longueur','largeur','client_id'
     ];
+
+    public function client(){
+        return $this->belongsTo(Client);
+    }
 }
